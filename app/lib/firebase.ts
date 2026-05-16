74030473a47cd8ce6abe3f574489ef3cdaafdc63
김용하 <kimyongha@gim-yonghaui-MacBookPro.local>
2026-05-16 13:12:01 -0400

    fix firebase realtime database

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCQUyhkpG4iuu77GTN3NvFrOH5deCAfrlM",
+  apiKey: "AIzaSyQVyhkpG4iuu77GTN3NvFrOH5deCAfrlM",
   authDomain: "sheep-game-e1aa0.firebaseapp.com",
+  databaseURL: "https://sheep-game-e1aa0-default-rtdb.firebaseio.com",
   projectId: "sheep-game-e1aa0",
   storageBucket: "sheep-game-e1aa0.firebasestorage.app",
   messagingSenderId: "1003450643999",
   appId: "1:1003450643999:web:dea1e25dcb243dac33c72a",
-  measurementId: "G-FKNQED719B"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const database = getDatabase(app);
